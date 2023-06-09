9815d5571650cacdd81c98901ea741a6c6492592
lodalee <131640279+lodalee@users.noreply.github.com>
2023-06-09 12:37:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-from flask import Flask, render_template, request, jsonify
-app = Flask(__name__)
-
-from pymongo import MongoClient
-client = MongoClient('mongodb+srv://sparta:test@cluster0.rgsaqvn.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
-
 @app.route('/')
 def home():
     return render_template('index.html')
